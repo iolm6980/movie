620b140a14c4afbbfb59ad337a241cfaa3bb8107
iolm6980 <133768355+iolm6980@users.noreply.github.com>
2024-02-05 19:22:10 +0900

    별점 구현

--- src/test/java/com/example/movie/serviceTests/MovieInfoServiceTests.java
+++ src/test/java/com/example/movie/serviceTests/MovieInfoServiceTests.java
@@ -1,6 +1,8 @@
 package com.example.movie.serviceTests;
 
 import com.example.movie.dto.MovieDTO;
+import com.example.movie.dto.MovieInfoDTO;
+import com.example.movie.entity.MovieInfo;
 import com.example.movie.repository.MovieInfoRepository;
 import com.example.movie.service.MovieInfoService;
 import com.example.movie.service.MovieService;
@@ -28,7 +30,7 @@ public class MovieInfoServiceTests {
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String today = dateFormat.format(date);
-        List<MovieDTO> list = movieInfoService.getMovie(today, 1L);
+        List<MovieInfoDTO> list = movieInfoService.getMovie(today, 1L);
         System.out.println(list);
     }
 }
